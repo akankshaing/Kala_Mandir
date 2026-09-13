916617ce56d9e874e13b4b1b67702268488f5971
Akanksha Singh <143275427+akankshaing@users.noreply.github.com>
2026-09-13 23:44:01 +0530

    Delete src directory

--- src/components/AdminRoute.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { Navigate, Outlet } from "react-router-dom";
-import { useAdminAuth } from "../context/AdminAuthContext.jsx";
-
-export default function AdminRoute() {
-  const { isAdmin } = useAdminAuth();
-  return isAdmin ? <Outlet /> : <Navigate to="/admin/login" replace />;
-}
